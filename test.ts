cdda0343a3c42e8a51ea6bc7de834b49721229b7
gregoryagu <greg.gum@gmail.com>
2015-12-05 16:52:13 -0500

    Create test.ts
    This is an example of using the wrapper. The selectedDate property is what will be updated by the Date Control and the binding engine. So, for example, if you have a contact entity with a birthdate, you would not have selectedDate here, but your entity.  Then you bind in the html to the entity.

---
+++ test.ts
@@ -0,0 +1,8 @@
+export class Test{
+    showDate() {
+        alert(this.selectedDate);
+    }
+    selectedDate:Date = new Date(2000,1,1);
+    
+    myContact: Contact:  //Get contact entity from your data-service and put here.
+}
